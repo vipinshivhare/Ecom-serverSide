2bccdcf746928bb7296533641d7d8c829e1f55b3
vipinshivhare <vipinshivhare804@gmail.com>
2025-08-08 22:25:30 +0530

    Initial

--- src/main/java/com/vipin/ecom_proj/model/Product.java
+++ src/main/java/com/vipin/ecom_proj/model/Product.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
